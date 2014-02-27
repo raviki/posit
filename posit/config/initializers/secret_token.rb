e5e299ce7ab0cbb1644a2833f6f5e4a9b8855a44
Ravi Kishor <p11ravikishorr@iimahd.ernet.in>
2014-02-27 16:25:11 +0530

    commit

---
+++ posit/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Posit::Application.config.secret_key_base = '02803ecf828bfac4ad9371b6964c65fc17566133c360fda92bcc09cf7725f088feea5e865f6069753eaec313098b0b6935eb8a72a2d0c7b4cda2768f816305d5'
